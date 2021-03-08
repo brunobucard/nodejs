17253572ccb3e227a553129d905795ecbc886c88
Bruno Bucard <brunobucar@gmail.com>
2021-03-08 15:19:45 -0300

    Params

--- src/index.js
+++ src/index.js
@@ -2,12 +2,49 @@ const express = require('express');
 
 const app = express();
 
-//Porta localhost:3333
+app.use(express.json());
 
-app.get("/", (request, response) =>{
-    return response.json({message: "Hello World Ignite!" });
+/**
+ * GET - Buscar um informação dentro do servidor
+ * POST - Inserir uma informação no servidor
+ * PUT - Alterar uma informação no servidor
+ * PATCH - Alterar uma informação específica
+ * DELETE - Deletar uma informação no servidor
+ */
+
+/**
+ * Tipos de parametros
+ * 
+ * Route Params => Identificar um recurso editar/deletar/buscar
+ * Query Params => Paginação / filtro 
+ * Body Params = > Os objetos inserção/alteração (JSON)
+ */
+
+app.get("/courses", (request, response) =>{
+    const query = request.query;
+    console.log(query);
+    return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
+app.post("/courses", (request, response) => {
+    const body = request.body;
+    console.log(body);
+    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+    const {id} = request.params;
+    console.log(id);
+    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json(["Curso 6", "Curso 2", "Curso 4"]);
+})
 
 app.listen(3333);
 
